04671b05725ff5b2c033fe9c1126f46c3abe08d3
gu <82009882+kugu98@users.noreply.github.com>
2023-01-10 22:39:15 +0900

    AOP

--- hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.sevice.MemberService;
@@ -21,6 +22,11 @@ public class SpringConfig {
     public MemberService memberService(){
         return new MemberService(memberRepository);
     }
+
+   /* @Bean
+    public TimeTraceAop timeTraceAop(){
+        return new TimeTraceAop();
+    }*/
 
    /* @Bean
     public MemberRepository memberRepository(){
