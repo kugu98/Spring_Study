2c5f769abe74f5577c980a694a01d296d5c19ee8
gu <82009882+kugu98@users.noreply.github.com>
2023-01-16 15:26:33 +0900

    새로운 할인 정책 개발

---
+++ core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,34 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+    @Test
+    @DisplayName("VIP 10 persent")
+    void vip_o(){
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000);
+
+    }
+    @Test
+    @DisplayName("VIP 10 persent fail")
+    void vip_x(){
+        //given
+        Member member = new Member(1L, "memberBASIC", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
+}
