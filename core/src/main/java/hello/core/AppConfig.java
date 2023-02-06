b393745eb0e4de986b1ac3fcb6bc6228fb35433c
gu <82009882+kugu98@users.noreply.github.com>
2023-02-06 15:45:05 +0900

    등록 Bean 찾기

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AppConfig {
+public class  AppConfig {
     @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
