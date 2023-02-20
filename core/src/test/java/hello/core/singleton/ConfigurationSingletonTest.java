37cd1f0e97794264b94597929998e89856ac9ec3
gu <82009882+kugu98@users.noreply.github.com>
2023-02-20 15:11:02 +0900

    Configuration byte code

--- core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -29,4 +29,10 @@ public class ConfigurationSingletonTest {
         Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
 
     }
+    @Test
+    void configurationDeep(){
+        ApplicationContext ac=new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean=ac.getBean(AppConfig.class);
+        System.out.println("bean.getClass() = " + bean.getClass());
+    }
 }
