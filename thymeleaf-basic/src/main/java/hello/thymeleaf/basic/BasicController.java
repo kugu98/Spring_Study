5046e0159f894fba288ce350ca64e54852b8fc20
gu <82009882+kugu98@users.noreply.github.com>
2023-09-06 17:30:09 +0900

    attribute

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -81,6 +81,10 @@ public class BasicController {
         model.addAttribute("data","Spring!");
         return "basic/operation";
     }
+    @GetMapping("/attribute")
+    public String attribute(){
+        return "basic/attribute";
+    }
     @Data
     static class User{
         private String username;
